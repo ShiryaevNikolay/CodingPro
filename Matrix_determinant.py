a7ce0b72dc62f1d5cb13ceaf3fa94f4b41d0b6b5
Nikolay Shiryaev <deendeeps@list.ru>
2019-04-07 16:04:56 +0300

    Added comments, documentation. Formatted the code.

--- Matrix_determinant.py
+++ Matrix_determinant.py
@@ -4,44 +4,52 @@ matrix = []
 
 while True:
     try:
-        n = int(input("Введите степень матрицы: "))
+        n = int(input("Введите порядок матрицы: "))
         if n > 0:
             break
     except ValueError:
         print("Некорректный ввод! Введите целое положительное число.")
-#============================================================================================
-for i in range(n):                              #
-    matrix.append([])                           # Заполнение матрицы
-    for j in range(n):                          # и вывод на экран
-        matrix[i].append(randint(-9, 10))       #
-    print(matrix[i])                            #
-#============================================================================================
-#print(matrix)
-print("\n")
+# ==========================================================================
+print("\nМатрица:\n")                                   # Заполнение матрицы
+for i in range(n):                                      # и вывод на экран
+    matrix.append([])                                   #
+    for j in range(n):                                  #
+        matrix[i].append(randint(-9, 10))               #
+    print("    ", matrix[i])                            #
+# ==========================================================================
 
-def determinant(i_det, det_matrix):
-    opred_matrix = 0
+
+""" Информация:
+        det_matrix - матрицы, детерминат которых будем находить
+        matrix_determinant - детерминант матрицы (дополнений матрицы)
+        i_string_matrix - номер элемента, для которого составляем дополнение
+        j_string_complement - номер строки элемента, который будет записан в дополнение
+        k_column_complement - номер столбца элемента, который будет записан в дополнение
+        complement_index - нумерация строк дополения """
+
+
+def determinant(det_matrix):
+    matrix_determinant = 0
     if len(det_matrix) == 1:
-        return det_matrix[i_det][i_det]
-    min_matrix = []
-    for i in range(i_det, len(det_matrix)):
-        index = 0
-        for j in range(i_det + 1, len(det_matrix)):
-            min_matrix.append([])
-            for k in range(i_det, len(det_matrix)):
-                if k != i:
-                    min_matrix[index].append(det_matrix[j][k])
-            print(min_matrix[index])
-            index += 1
-            #determinant(0, det_matrix)
-        if i % 2 == 0:
-            opred_matrix = opred_matrix + determinant(0, min_matrix) * det_matrix[0][i]
-        else:
-            opred_matrix = opred_matrix - determinant(0, min_matrix) * det_matrix[0][i]
-        print(opred_matrix)
-        min_matrix = []
-    return opred_matrix
-        #print("\n")
+        return det_matrix[0][0]
+    matrix_complement = []
+    # ==============================================================================================================================
+    for i_string_matrix in range(len(det_matrix)):                                                                      # Вычисляем
+        complement_index = 0                                                                                            # дополнения
+        for j_string_complement in range(1, len(det_matrix)):                                                           # для
+            matrix_complement.append([])                                                                                # каждого
+            for k_column_complement in range(len(det_matrix)):                                                          # элемента
+                if k_column_complement != i_string_matrix:                                                              # первной
+                    matrix_complement[complement_index].append(det_matrix[j_string_complement][k_column_complement])    # строки
+            complement_index += 1                                                                                       #
+        # ==============================================================================================================================
+        if i_string_matrix % 2 == 0:                                                                                       # Подчитываем
+            matrix_determinant = matrix_determinant + determinant(matrix_complement) * det_matrix[0][i_string_matrix]      # детерминант
+        else:                                                                                                              # для
+            matrix_determinant = matrix_determinant - determinant(matrix_complement) * det_matrix[0][i_string_matrix]      # матриц
+        matrix_complement = []                                                                                             #
+        # ==============================================================================================================================
+    return matrix_determinant
 
 
-determinant(0, matrix)
+print("\nДетерминант матрицы: ", determinant(matrix))
