28f1e3079d05af142be7ad0e56c506a232bfcdc9
Nikolay Shiryaev <deendeeps@list.ru>
2019-04-07 22:20:07 +0300

    Added decimal representation.

--- DEC to BIN.py
+++ DEC to BIN.py
@@ -1,11 +1,48 @@
-def binaryRepresentation(x, h):
+from random import randint
+import re
+
+while True:
+    try:
+        n = int(input("Введите число, которое нужно привести в двоичный вид: "))
+        if n > 0:
+            break
+    except ValueError:
+        print("Некорректный ввод! Введите целое положительное число.")
+
+
+def binary_representation(x, h):
     c = x % 2
     x = x // 2
     h += str(c)
     if x > 0:
-        return binaryRepresentation(x, h)
+        return binary_representation(x, h)
     else:
-        return print(h[::-1])
+        return print("\n", h[::-1], "\n")
 
 
-binaryRepresentation(125, "")
+def decimal_representation(x):
+    string = str(x)
+    number = 0
+    for i in range(len(string)):
+        number += (2 ** (len(string) - 1 - i)) * int(string[i])
+    return print("\n", number, "\n")
+
+
+binary_representation(n, "")
+
+
+while True:
+    try:
+        n = int(input("Введите число в двоичном виде, чтобы привести в десятичный: "))
+        if n > 0:
+            string = str(n)
+            check = r"[23456789]"
+            if re.search(check, string):
+                n = 1.2
+            else:
+                break
+    except ValueError:
+        print("Некорректный ввод! Чило должно состоять из 0 и 1.")
+
+
+decimal_representation(n)
